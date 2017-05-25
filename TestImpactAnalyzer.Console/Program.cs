b136ae495fa6fb94907d23ab192102b55ffe383a
Dmitry Kolomiyets <dmitry@unity3d.com>
2017-05-26 01:36:38 +0200

    Added mercurial changes getting support

--- TestImpactAnalyzer.Console/Program.cs
+++ TestImpactAnalyzer.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using TestImpactAnalyzer.Lib;
 
@@ -8,14 +10,24 @@ namespace TestImpactAnalyzer.Console
     {
         static void Main()
         {
-            var className = "Class1";
-            var solutionPath = "C:\\Projects\\unity3d\\tia\\TestImpactAnalyzer.sln";
+            //var className = "Class1";
+            //var solutionPath = "C:\\Projects\\unity3d\\tia\\TestImpactAnalyzer.sln";
+
+            var workingFolder = "C:\\Projects\\unity3d\\unity";
+            var solutionPath = $"{workingFolder}\\Projects\\CSharp\\Unity.CSharpProjects.gen.sln";
+
+            var mercurialClient = new MercurialClient(workingFolder);
+            var changes = mercurialClient.GetChangedFiles();
+
+            var classPath = changes[0];
+            var className = Path.GetFileNameWithoutExtension(classPath);
 
             System.Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Searching for class \"{0}\" reference in solution {1} ", className, Path.GetFileName(solutionPath));
 
             var finder = new ReferenceFinder(solutionPath);
-            var locations = finder.FindClassUsages($"C:\\Projects\\unity3d\\tia\\TestImpactAnalyzer.Lib.Samples\\{className}.cs", $"{className}");
+            //var locations = finder.FindClassUsages($"C:\\Projects\\unity3d\\tia\\TestImpactAnalyzer.Lib.Samples\\{className}.cs", $"{className}");
+            var locations = finder.FindClassUsages(classPath, className);
             System.Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Found references in files.");
             System.Console.ResetColor();
@@ -28,9 +40,31 @@ namespace TestImpactAnalyzer.Console
             System.Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Found references in unit tests.");
             System.Console.ResetColor();
+            var testsAssemlblies = new List<string>();
+            var testsNames = new List<string>();
             foreach (var location in unitTests)
             {
                 System.Console.WriteLine("File: " + location.Document.Name);
+                testsAssemlblies.Add(location.Document.Project.OutputFilePath);
+                testsNames.Add($"test =~ /{Path.GetFileNameWithoutExtension(location.Document.Name)}/");
+            }
+
+            var testAssembliesCombined = string.Join(" ", testsAssemlblies);
+            var testsExpression = string.Join(" || ", testsNames);
+
+            var runTestCommandProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = $"{workingFolder}\\External\\NUnit\\nunit-console.exe",
+                    Arguments = $"{testAssembliesCombined} --where \"{testsExpression}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            runTestCommandProcess.Start();
+            while (!runTestCommandProcess.StandardOutput.EndOfStream) {
+                var line = runTestCommandProcess.StandardOutput.ReadLine();
+                System.Console.WriteLine(line);
             }
         }
     }
---
+++ TestImpactAnalyzer.Lib/MercurialClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestImpactAnalyzer.Lib
+{
+    public class MercurialClient
+    {
+        private readonly string _workingFolder;
+
+        public MercurialClient(string workingFolder)
+        {
+            _workingFolder = workingFolder;
+        }
+
+        public string[] GetChangedFiles()
+        {
+            var changedFiles = new List<string>();
+            var statusCommandProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "hg",
+                    Arguments = "status",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = _workingFolder
+                }
+            };
+            statusCommandProcess.Start();
+            while (!statusCommandProcess.StandardOutput.EndOfStream) {
+                var line = statusCommandProcess.StandardOutput.ReadLine();
+                var changedFilePath = line.Split(' ')[1];
+                var changedFileFullPath = Path.Combine(_workingFolder, changedFilePath);
+                if (Path.GetExtension(changedFileFullPath) != ".cs")
+                {
+                    continue;
+                }
+                changedFiles.Add(changedFileFullPath);
+            }
+            return changedFiles.ToArray();
+        }
+    }
+}
