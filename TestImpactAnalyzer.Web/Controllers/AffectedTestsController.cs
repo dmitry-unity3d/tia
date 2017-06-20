c7f08981442478941338b2d68c81fcd9694d7692
Dmitry Kolomiyets <dmitry@unity3d.com>
2017-06-20 16:11:37 +0100

    Web Api project added

---
+++ TestImpactAnalyzer.Web/Controllers/AffectedTestsController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Web.Http;
+using Newtonsoft.Json;
+using TestImpactAnalyzer.Lib;
+
+namespace TestImpactAnalyzer.Web.Controllers
+{
+    public class AffectedTestsController : ApiController
+    {
+        // GET api/affected-tests/5
+        public string Get(string id)
+        {
+            var workingFolder = "C:\\Projects\\unity3d\\unity";
+            var solutionPath = $"{workingFolder}\\Projects\\CSharp\\Unity.CSharpProjects.gen.sln";
+            var mercurialClient = new MercurialClient(workingFolder);
+            var changes = mercurialClient.GetChangedFiles(id);
+            var classPath = changes[0];
+            var className = Path.GetFileNameWithoutExtension(classPath);
+            var finder = new ReferenceFinder(solutionPath);
+            var locations = finder.FindAffectedTests(classPath, className);
+            //var locations = new[] { @"Tests\Unity.PureCSharpTests\FileMirroringTests" };
+            return JsonConvert.SerializeObject(locations);
+        }
+    }
+}
---
+++ TestImpactAnalyzer.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestImpactAnalyzer.Web.WebApiApplication" Language="C#" %>
