55cb197c19b1f774f2952c1fcf165accb8ce55d9
Dmitry Kolomiyets <dmitry@unity3d.com>
2017-06-21 20:36:08 +0100

    CommandLineParameters class and its tests added

---
+++ TestImpactAnalyzer.Lib/Utils/CommandLineParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Util;
+
+namespace TestImpactAnalyzer.Lib.Utils
+{
+    public class CommandLineParameters
+    {
+        public string SolutionPath { get; set; }
+
+        public bool RunTests { get; set; }
+
+        public static CommandLineParameters Parse(string[] args)
+        {
+            var parsedParameters = new CommandLineParameters();
+            parsedParameters.RunTests = args.Contains("--run-tests");
+            return parsedParameters;
+        }
+    }
+}
---
+++ TestImpactAnalyzer.UnitTests/CommandLineParametersTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestImpactAnalyzer.Lib.Utils;
+
+namespace TestImpactAnalyzer.UnitTests
+{
+    [TestClass]
+    public class CommandLineParametersTests
+    {
+        [TestMethod]
+        public void Parse_RunTestsParameterExists_RunTestsIsTrue()
+        {
+            var actualParameters = CommandLineParameters.Parse(new[] { "--run-tests" });
+
+            Assert.IsTrue(actualParameters.RunTests);
+        }
+    }
+}
